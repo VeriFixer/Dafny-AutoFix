3863d1ab6a75f763bcc350722655f327226c0399
Isabel Amaral <isabel.andre.amaral@gmail.com>
2025-11-20 21:08:46 -0100

    Initial setup

---
+++ snapshot-generator/SnapshotGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Dafny;
+using Microsoft.Dafny.Plugins;
+using PluginConfiguration = Microsoft.Dafny.LanguageServer.Plugins.PluginConfiguration;
+
+namespace SnapshotGenerator;
+
+public class SnapshotGenerator : PluginConfiguration
+{ 
+   public override Rewriter[] GetRewriters(ErrorReporter reporter) { 
+       return [];
+   }
+}
