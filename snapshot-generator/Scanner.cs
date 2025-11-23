03be2136c99e1caae568abc02e54cf4fba6a0f76
Isabel Amaral <isabel.andre.amaral@gmail.com>
2025-11-23 16:07:59 +0000

    Collect possible calls to argumentless boolean queries on objects

--- snapshot-generator/Scanner.cs
+++ snapshot-generator/Scanner.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Dafny;
 using Microsoft.Dafny.Plugins;
+using Type = Microsoft.Dafny.Type;
 
 namespace SnapshotGenerator;
 
@@ -7,13 +8,21 @@ public class Scanner(ErrorReporter reporter) : Rewriter(reporter)
 {
     private readonly List<Expression> _programAbstractions = [];
     private readonly List<Expression> _defaultClassAbstractions = [];
+    private readonly List<(string, string)> _allBoolArgumentlessPreds = [];
     private bool _insideDefaultClass;
+    private bool _insideFaultyTopLevelDecl;
+    private string _currentTopLevelDecl = "";
     
     public override void PreResolve(ModuleDefinition module) {
         Scan(module);
+    }
+
+    public override void PostResolve(ModuleDefinition module) {
+        // collect additional predicates from faulty method after fully parsing the AST
+        VisitFaultyMethod();
         
         if (SnapshotGenerator.FaultyMethod == null) return;
-        using StreamWriter sw = File.CreateText("abstractions.txt");
+        using StreamWriter sw = File.AppendText("abstractions.txt");
         foreach (var expr in _programAbstractions) {
             var line = expr.ToString();
             sw.WriteLine(line);
@@ -21,10 +30,14 @@ public class Scanner(ErrorReporter reporter) : Rewriter(reporter)
     }
 
     private void Scan(ModuleDefinition module) {
+        // first AST parse
         HandleDefaultClassDecl(module);
         HandleSourceDecls(module);
     }
 
+    /// --------------------------
+    /// Group of AST node visitors
+    /// --------------------------
     private void HandleDefaultClassDecl(ModuleDefinition module) {
         if (module.DefaultClass == null) return;
         
@@ -41,15 +54,16 @@ public class Scanner(ErrorReporter reporter) : Rewriter(reporter)
         foreach (var decl in module.SourceDecls) {
             if (decl is not TopLevelDeclWithMembers declWithMembers) // includes class, trait, datatype, etc.
                 continue;
-            // visit if faulty method belongs to declaration
-            if (!(decl.StartToken.line <= SnapshotGenerator.ViolationLine &&
-                  decl.EndToken.line >= SnapshotGenerator.ViolationLine))
-                return;
             HandleMemberDecls(declWithMembers);
         }
     }
     
-    private void HandleMemberDecls(TopLevelDeclWithMembers decl) {
+    private void HandleMemberDecls(TopLevelDeclWithMembers decl) {       
+        _currentTopLevelDecl = decl.Name;
+        if (decl.StartToken.line <= SnapshotGenerator.ViolationLine && 
+            decl.EndToken.line >= SnapshotGenerator.ViolationLine)
+            _insideFaultyTopLevelDecl = true;
+        
         foreach (var member in decl.Members) {
             if (member is Method m) { // includes constructor
                 HandleMethod(m);  
@@ -57,6 +71,7 @@ public class Scanner(ErrorReporter reporter) : Rewriter(reporter)
                 HandleFunction(func);
             }
         }
+        _insideFaultyTopLevelDecl = false;
     }
     
     private void HandleMethod(Method method) {
@@ -69,11 +84,53 @@ public class Scanner(ErrorReporter reporter) : Rewriter(reporter)
     private void HandleFunction(Function function) {
         // collect argumentless boolean predicates
         if (function.ResultType is BoolType && function.Ins.Count == 0 && function.Body != null) {
-            if (!_insideDefaultClass) {
+            if (!_insideDefaultClass && _insideFaultyTopLevelDecl) {
                 _programAbstractions.Add(function.Body);
             } else {
                 _defaultClassAbstractions.Add(function.Body);
             }
+            // argumentless boolean predicates callable by objects of type _currentTopLevelDecl
+            _allBoolArgumentlessPreds.Add((_currentTopLevelDecl, function.Name));
+        }
+    }
+    
+    /// --------------------------
+    /// Faulty method
+    /// --------------------------
+    private void VisitFaultyMethod() {
+        var faultyMethod = SnapshotGenerator.FaultyMethod;
+        if (faultyMethod == null || faultyMethod.Body == null)
+            return;
+
+        CollectBoolArgumentlessCalls(faultyMethod);
+        foreach (var stmt in faultyMethod.Body.Body) {
+            if (stmt is VarDeclStmt vDeclStmt) 
+                CollectBoolArgumentlessCalls(vDeclStmt);
+        }
+    }
+
+    private void CollectBoolArgumentlessCalls(Method faultyMethod) {
+        foreach (var input in faultyMethod.Ins) {
+            CollectBoolArgumentlessCalls(input.Origin, input.Type, input.Name);
+        }
+    }
+
+    private void CollectBoolArgumentlessCalls(VarDeclStmt vDeclStmt) {
+        foreach (var lhs in vDeclStmt.Locals) {
+            CollectBoolArgumentlessCalls(lhs.Origin, lhs.Type, lhs.Name);
+        }
+    }
+
+    private void CollectBoolArgumentlessCalls(IOrigin origin, Type type, string objectName) {
+        if (!(type is UserDefinedType)) return;
+        var applicablePreds = _allBoolArgumentlessPreds.Where(
+            (pred) => pred.Item1 == type.ToString()
+        );
+        foreach (var pred in applicablePreds) {
+            var lhs = new NameSegment(origin, objectName, null);
+            var suffixName = new Name(pred.Item2);
+            var callExpr = new ExprDotName(origin, lhs, suffixName, null);
+            _programAbstractions.Add(callExpr);
         }
     }
 }
