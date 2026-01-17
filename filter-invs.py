ac5c58375073acc217a39ca64959b4be1c55b354
Isabel Amaral <isabel.andre.amaral@gmail.com>
2026-01-17 17:28:42 +0000

    Created python script to compute differente between failing and passing invariants

---
+++ filter-invs.py
@@ -0,0 +1,51 @@
+def read_invariants(passing):
+    invs = {}
+
+    with open("inv-pass.inv" if passing else "inv-fail.inv", 'r') as file:
+        location = None
+        new_location = False
+        location_invs = []
+        for line in file:
+            if new_location:
+                if (location != None):
+                    invs[location] = location_invs
+                location = line.strip()
+                new_location = False
+                location_invs = []
+            elif line.strip() == "===========================================================================":
+                new_location = True
+            elif location != None:
+                location_invs.append(line.strip())
+
+    return invs
+
+
+def compute_invariant_difference(passing_invs, failing_invs):
+    filtered_invs = {}
+
+    for location, invs in failing_invs.items():
+        location_invs = []
+
+        for inv in invs:
+            if inv not in passing_invs[location]:
+                location_invs.append(inv)
+        filtered_invs[location] = location_invs
+
+    return filtered_invs
+
+
+def main():
+    passing_invs = read_invariants(True)
+    failing_invs = read_invariants(False)
+    filtered_invs = compute_invariant_difference(passing_invs, failing_invs)
+    
+    with open("inv.inv", "w") as f:
+        for location, invs in filtered_invs.items():
+            f.write(location + "\n")
+            for inv in invs:
+                f.write(inv + "\n")
+        f.close()
+
+
+if __name__ == "__main__":
+    main()
